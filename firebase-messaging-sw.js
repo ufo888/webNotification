b32fbcfe7dbfe91a1d08268966998a8be9e2163b
eric <ufo888@gmail.com>
2022-12-06 16:06:09 +0800

    1st init

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,39 @@
+importScripts("https://www.gstatic.com/firebasejs/7.16.1/firebase-app.js");
+importScripts(
+    "https://www.gstatic.com/firebasejs/7.16.1/firebase-messaging.js",
+);
+// For an optimal experience using Cloud Messaging, also add the Firebase SDK for Analytics.
+importScripts(
+    "https://www.gstatic.com/firebasejs/7.16.1/firebase-analytics.js",
+);
+
+// Initialize the Firebase app in the service worker by passing in the
+// messagingSenderId.
+firebase.initializeApp({
+    messagingSenderId: "866228480011",
+    apiKey: "AIzaSyDK9b2f3jzb5DLcN7V6KXXx79Iz4r30a2k",
+    projectId: "r8546546",
+    appId: "1:866228480011:web:63e47a6de08e5d50b73f0e",
+});
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler(function(payload) {
+    console.log(
+        "[firebase-messaging-sw.js] Received background message ",
+        payload,
+    );
+    // Customize notification here
+    const notificationTitle = "Background Message Title";
+    const notificationOptions = {
+        body: "Background Message body.",
+        icon: "/itwonders-web-logo.png",
+    };
+
+    return self.registration.showNotification(
+        notificationTitle,
+        notificationOptions,
+    );
+});
